ef1b6bdda3fa54b992ab6913f83b8256e1f15173
Lucas FC <lucasfernandes_222@hotmail.com>
2022-11-29 19:03:53 -0300

    crud

---
+++ Products/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Products.Enum;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Products.Models
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        
+        public string Name { get; set; }
+
+        
+        public Fabricante Fabricante { get; set; }
+
+        
+        public Categoria Categoria { get; set; }
+
+
+        public int Preco { get; set; }
+    }
+}
